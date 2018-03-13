271caf73939675f797e978d9368f0fffb2252747
league <league@wts13.attlocal.net>
2018-03-13 16:58:57 -0700

    •VotingBooth •AnimalFarm

--- src/methods/AnimalFarm.java
+++ src/methods/AnimalFarm.java
@@ -1,10 +1,11 @@
 package methods;
 
 //Copyright (c) The League of Amazing Programmers 2013-2017
+
 //Level 0
 
-
 import java.io.File;
+import java.lang.annotation.Repeatable;
 
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
@@ -13,50 +14,59 @@ import javax.swing.JOptionPane;
 
 public class AnimalFarm {
 
-AnimalFarm() {
-	/*
-	 * 1. Ask the user which animal they want, then play the sound of that
-	 * animal.
-	 */
-
-	/* 2. Make it so that the user can keep entering new animals. */
-}
-
-void playMoo() {
-	playNoise(mooFile);
-}
-
-void playQuack() {
-	playNoise(quackFile);
-}
-
-void playWoof() {
-	playNoise(woofFile);
-}
-
-String quackFile = "quack.wav";
-String mooFile = "moo.wav";
-String woofFile = "woof.wav";
-String meowFile = "meow.wav";
-String llamaFile = "llama.wav";
-
-
-/* Ignore this stuff */
-
-public void playNoise(String soundFile) {
-	try {
-		 Clip clip = AudioSystem.getClip();
-		 clip.open(AudioSystem.getAudioInputStream(getClass().getResource(soundFile)));
-		 clip.start();
-		 Thread.sleep(3400);
-	} catch (Exception ex) {
-  	ex.printStackTrace();
+	AnimalFarm() {
+		/*
+		 * 1. Ask the user which animal they want, then play the sound of that animal.
+		 */
+	String name = JOptionPane.showInputDialog(null, "Pick one: Cow, Duck, or a Dog.");
+		/* 2. Make it so that the user can keep entering new animals. */
+	
+	if (name.equals("Cow")){
+		playMoo();
 	}
-}
+	if (name.equals("Duck")){
+	playQuack();
+	}
+	if (name.equals("Dog"))
+	playWoof();
+	
+	}
 
-public static void main(String[] args) {
-	new AnimalFarm();
-}
+	
+
+	void playMoo() {
+		playNoise(mooFile);
+	}
+
+	void playQuack() {
+		playNoise(quackFile);
+	}
+
+	void playWoof() {
+		playNoise(woofFile);
+	}
+
+	String quackFile = "quack.wav";
+	String mooFile = "moo.wav";
+	String woofFile = "woof.wav";
+	String meowFile = "meow.wav";
+	String llamaFile = "llama.wav";
+
+	/* Ignore this stuff */
+
+	public void playNoise(String soundFile) {
+		try {
+			Clip clip = AudioSystem.getClip();
+			clip.open(AudioSystem.getAudioInputStream(getClass().getResource(soundFile)));
+			clip.start();
+			Thread.sleep(3400);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		new AnimalFarm();
+	}
 
 }
-
---
+++ src/methods/VotingBooth.java
@@ -0,0 +1,24 @@
+package methods;
+
+import javax.swing.JOptionPane;
+
+public class VotingBooth {
+
+	public static void main(String[] args) {
+	
+
+String Age = JOptionPane.showInputDialog("How old are you?");
+
+int age = Integer.parseInt(Age);
+	if(age>18)
+	{
+		JOptionPane.showInputDialog(null, "Who should the next president be?");
+JOptionPane.showMessageDialog(null, "Good choice.");
+	}
+	
+	else{
+		JOptionPane.showMessageDialog(null, "Sorry, you can't vote, and "
+				+ "nobody cares what you think.");
+	}
+	}
+}
